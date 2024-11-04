ddaebe13cb30fbf048cad86074ed1a04df26179f
Derrick <pshubomi@gmail.com>
2024-11-04 16:42:58 +0100

    installed the mailer file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MailerService } from './mailer/mailer.service';
+import { MailerModule } from './mailer/mailer.module';
 
 @Module({
-  imports: [],
+  imports: [MailerModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MailerService],
 })
 export class AppModule {}
